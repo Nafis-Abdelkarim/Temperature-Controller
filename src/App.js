67024a65f436cb6a8f2da3d220b39d64d917363e
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-04-15 01:03:27 +0100

    Update my code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  // Set our hooks
+  const [temperatureValue, setTemperatureValue] = useState(10)
+  const [temperatureColor, setTemperatureColor] = useState("cold")
+
+  //create increment function 
+  const incrementTemperature = () => {
+    //limit the increase to 30
+    if(temperatureValue === 30) return 
+    const newTemperature = temperatureValue + 1
+
+    if(newTemperature >= 15){
+      setTemperatureColor('hot') //if the temperature is more than 15 we change the state of display to hot
+    }
+  
+    setTemperatureValue(newTemperature)
+  }
+
+  //create drecrement function
+  const decrementTemprature = () => {
+    //limit the decrease till 0
+    if(temperatureValue === 0) return  
+    const newTemperature = temperatureValue - 1
+
+    if(newTemperature < 15){
+      setTemperatureColor('cold') //if the temperature is less than 15 we change the state of display to cold
+    }
+
+    setTemperatureValue(newTemperature)
+  }
+
+  return(
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°C</div>
+      </div>
+      <div className="button-container">
+      <button onClick={() => incrementTemperature()}>+</button>
+      <button onClick={() => {setTemperatureValue(10)}}>10</button> {/* Rest the default value wich is 10 */}
+      <button onClick={() => decrementTemprature()}>-</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
